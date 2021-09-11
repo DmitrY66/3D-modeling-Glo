5ac09948e91448d3af93edcbe942209a0447aa72
Dmitri Gorun <iii448@mail.ru>
2021-09-12 01:43:32 +0500

    lesson21-delegation-no-changes-dop

--- src/modules/toggleMenu.js
+++ src/modules/toggleMenu.js
@@ -10,19 +10,32 @@
 'use strict';
 
 const toggleMenu = () => {
-  const btnMenu = document.querySelector('.menu');
-  const menu = document.querySelector('menu');
+  const bodyMenu = document.querySelector('body');
+  const menuTop = document.querySelector('menu');
 
-  const handlerMenu = () => {
-    menu.classList.toggle('active-menu');
-  };
+  const div = document.createElement('div');
+  div.className = 'shade';
+  div.style.width = '100%';
+  div.style.height = '100vh';
+  div.style.background = '#00000063';
+  div.style.position = 'fixed';
+  div.style.top = '0';
+  div.style.display = 'none';
+  bodyMenu.append(div);
 
-  btnMenu.addEventListener('click', handlerMenu);
-
-  menu.addEventListener('click', (e) => {
+  bodyMenu.addEventListener('click', (e) => {
     let target = e.target;
+    if (target.closest('.menu')) {
+      menuTop.classList.toggle('active-menu');
+      div.style.display = 'block';
+    }
     if (target.matches('a')) {
-      menu.classList.remove('active-menu');
+      menuTop.classList.remove('active-menu');
+      div.style.display = 'none';
+    }
+    if (target.matches('.shade')) {
+      menuTop.classList.remove('active-menu');
+      div.style.display = 'none';
     }
   });
 };
